f2567e1ed1c0ce7b6aff21ac8d8eb199f71c6f9c
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-13 19:41:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,6 +20,7 @@ class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
 
   def add_song(song)
     @songs << song
